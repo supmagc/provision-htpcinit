c2f5d624d234e77e54018b97d6ac8a0cfc521b48
supmagc <supmagc@gmail.com>
2016-06-29 22:29:08 +0200

    Initial commit

---
+++ main.sh
@@ -0,0 +1,28 @@
+# Install lx keymap manager
+sudo apt-get install lxkeymap
+
+#force locale and keymap settings
+
+#install graphics-drivers ppa
+
+#install nvidia graphics drivers
+
+# install pulse
+sudo apt-get install pulseaudio pavucontrol
+
+# add kodi ppa
+
+#install kodi
+sudo apt-get install kodi
+
+#disable unneeded xsessions
+
+#create custom xsession to always enable openbox
+
+#install steam
+
+#install lirc from source
+
+#install lirc config
+
+#install samba config
