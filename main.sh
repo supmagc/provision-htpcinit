e0c018fee5656ec8fad86ec3a88e6ccd84f1740a
supmagc <supmagc@gmail.com>
2016-07-09 15:00:26 +0200

    Samba and variables progress

--- main.sh
+++ main.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
+source templates/default.conf
+if [ -f config.conf ]; then
+  source config.conf
+fi
+
 # Add ppa's
 sudo add-apt-repository ppa:team-xbmc/ppa
 sudo add-apt-repository ppa:graphics-drivers/ppa
 
 # Update apt
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-update -y
 
 # Install additional software
-sudo apt-get install lxkeymap
-sudo apt-get install pulseaudio pavucontrol
-sudo apt-get install nvidia-graphics-driver-364 vdpauinfo
-sudo apt-get install kodi
-sudo apt-get install samba
+sudo apt-get install -y openssl
+sudo apt-get install -y lxkeymap
+sudo apt-get install -y pulseaudio pavucontrol
+sudo apt-get install -y nvidia-graphics-driver-364 vdpauinfo
+sudo apt-get install -y kodi
+sudo apt-get install -y samba
+
+#create and configure ssl
+
+#configure network
 
 #force locale and keymap settings
-
-#install graphics-drivers ppa
-
-#install nvidia graphics drivers
 
 #disable unneeded xsessions
 
@@ -32,3 +39,5 @@ sudo apt-get install samba
 #install lirc config
 
 #install samba config
+sudo sed 's/{HOSTNAME}/$HOSTNAME/:s/{WORKGROUO}/$WORKGROUP/' templates/smb.conf > /etc/samba/smb.conf
+sudo service samba restart
