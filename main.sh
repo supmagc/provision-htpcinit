71bfa4a636b63a9caf64dcef21d5d3987ae27e77
supmagc <supmagc@gmail.com>
2017-09-14 22:08:50 +0200

    HtpcInit directory-name change

--- main.sh
+++ main.sh
@@ -327,8 +327,8 @@ systemctl restart nmbd.service
 # Copy from backup
 mkdir -p ~/Artwork
 mkdir -p ~/Cinema
-smbclient //$NAS_IP/Backup $NAS_PASSWORD -U=$NAS_USERNAME -c='prompt off;recurse on;cd HTPCINIT\Artwork\;lcd /home/jelle/Artwork/;mget *'
-smbclient //$NAS_IP/Backup $NAS_PASSWORD -U=$NAS_USERNAME -c='prompt off;recurse on;cd HTPCINIT\Cinema\;lcd /home/jelle/Cinema/;mget *'
+smbclient //$NAS_IP/Backup $NAS_PASSWORD -U=$NAS_USERNAME -c='prompt off;recurse on;cd HtpcInit\Artwork\;lcd /home/jelle/Artwork/;mget *'
+smbclient //$NAS_IP/Backup $NAS_PASSWORD -U=$NAS_USERNAME -c='prompt off;recurse on;cd HtpcInit\Cinema\;lcd /home/jelle/Cinema/;mget *'
 
 # Configure network
 hostname "$HOSTNAME"
