66ecf88fc344c4463c31a1a2011009ac60bf5709
supmagc <supmagc@gmail.com>
2019-12-02 21:35:33 +0100

    Add localhost ipv4

--- main.sh
+++ main.sh
@@ -396,6 +396,7 @@ set_rights /home/$USERNAME/Cinema
 hostname "$HOSTNAME"
 sed -i "/127.0.0.1/d" /etc/hosts
 sed -i "1 i 127.0.0.1\t$HOSTNAME.$DOMAIN\t$HOSTNAME" /etc/hosts
+sed -i "1 i 127.0.0.1\tlocalhost" /etc/hosts
 resolvconf -u
 nmcli connection modify "$NET_CONN" ipv4.method "manual" ipv4.dns "$NET_DNS" ipv4.addresses "$NET_IP" ipv4.gateway "$NET_GATE"
 systemctl restart network-manager.service
