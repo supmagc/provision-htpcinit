964b423c9f2f01b51134c2682483f6e9b4b5ab33
supmagc <supmagc@gmail.com>
2021-10-09 23:03:53 +0200

    lirc fixes

--- lirc.sh
+++ lirc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install autoconf automake libtool build-essential python3 python3-dev python3-setuptools kmod pkg-config xsltproc linux-headers-$(uname -r)
+sudo apt-get install autoconf automake libtool build-essential python3 python3-dev python3-setuptools kmod pkg-config xsltproc linux-headers-$(uname -r) libsystemd-dev
 
 git clone https://github.com/supmagc/lirc-mplay.git
 cd lirc-mplay/
@@ -9,7 +9,11 @@ cd lirc-mplay/
 ./configure --with-x --with-driver="mplay2" --prefix=""
 
 make
+# Sometimes, a doc file is symlinked
+git status | grep typechange | awk '{print $2}' | xargs git checkout
 sudo make install
+
+sudo systemctl enable lircd
 
 cd ../
 rm -R lirc-mplay
